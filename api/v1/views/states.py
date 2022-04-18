831da62cacbbdcb732cef37570ccdce4ba590072
YesseJr <thonnieyesse@hotmail.com>
2022-04-18 14:20:36 +0300

    States view

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,102 +1,57 @@
 #!/usr/bin/python3
-"""Module containing views for State objects """
+"""
+Module containing views for State objects
+"""
 from api.v1.views import app_views
-from flask import jsonify, abort, request
-from models import storage
-from models.state import State
+from flask import abort, jsonify, make_response, request
+from flasgger import Swagger, swag_from
+from models import storage, CNC
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
+@app_views.route('/states', methods=['GET', 'POST'])
+@swag_from('swagger_yaml/states_no_id.yml', methods=['GET', 'POST'])
+def states_no_id():
     """
-    Retrieves the list of all State objects
-    Returns:
-        A list of dictionaries representing State Objects in JSON format.
+        states route to handle http method for requested states no id provided
     """
-    states = []
-    for state in storage.all("State").values():
-        states.append(state.to_dict())
-    return jsonify(states)
+    if request.method == 'GET':
+        all_states = storage.all('State')
+        all_states = list(obj.to_json() for obj in all_states.values())
+        return jsonify(all_states)
+
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        if req_json.get("name") is None:
+            abort(400, 'Missing name')
+        State = CNC.get("State")
+        new_object = State(**req_json)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_state_by_id(state_id=None):
-    """ Retrieves a state object by state_id
-        Args:
-            state_id (str): The UUID4 string representing a State object.
-        Returns:
-            A state object in JSON format.
-            Raise 404 error if no match for state_id is found.
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+@swag_from('swagger_yaml/states_id.yml', methods=['PUT', 'GET', 'DELETE'])
+def states_with_id(state_id=None):
     """
-    if state_id:
-        for state in storage.all("State").values():
-            if state.id == state_id:
-                return jsonify(state.to_dict())
-        abort(404)
-
-
-@app_views.route('/states/<string:state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_state_by_id(state_id=None):
-    """ Deletes a state object by state_id
-        Args:
-            state_id (str): The UUID4 string representing a State object.
-        Returns:
-            An empty dictionary with the status code 200.
-            Raise 404 error if no match for state_id is found.
+        states route to handle http method for requested state by id
     """
-    state_obj = storage.get("State", state_id)
+    state_obj = storage.get('State', state_id)
     if state_obj is None:
-        abort(404)
-    state_obj.delete()
-    storage.save()
-    return jsonify({}), 200
+        abort(404, 'Not found')
 
+    if request.method == 'GET':
+        return jsonify(state_obj.to_json())
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
-    """ Creates a state object
-        Returns:
-            A dictionary of the state object with status code 201.
-            If HTTP body request is not valid JSON, raise a 400 error with the
-                message "Not a JSON".
-            If dictionary doesn't contain the key name, raise a 400 error with
-                message "Missing name".
-    """
-    if request.json is None:
-        return "Not a JSON", 400
-    fields = request.get_json()
-    if fields.get('name') is None:
-        return "Missing name", 400
-    new_state = State(**fields)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
+    if request.method == 'DELETE':
+        state_obj.delete()
+        del state_obj
+        return jsonify({})
 
-
-@app_views.route('/states/<string:state_id>', methods=['PUT'],
-                 strict_slashes=False)
-def update_state(state_id=None):
-    """ Updates a state object by state_id
-        Args:
-            state_id (str): The UUID4 string representing a State object.
-        Returns:
-            A state object in JSON format with the status code 200.
-            If no object with matching state_id is found, 404 error is raised.
-            If HTTP body request is not valid JSON, raise a 400 error with the
-                message "Not a JSON".
-            If dictionary doesn't contain the key name, raise a 400 error with
-                message "Missing name".
-    """
-    if request.json is None:
-        return "Not a JSON", 400
-    fields = request.get_json()
-    state_obj = storage.get("State", state_id)
-    if state_obj is None:
-        abort(404)
-    for key in fields:
-        if key not in ['id', 'created_at', 'updated_at']:
-            if hasattr(state_obj, key):
-                setattr(state_obj, key, fields[key])
-    state_obj.save()
-    return jsonify(state_obj.to_dict()), 200
+    if request.method == 'PUT':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        state_obj.bm_update(req_json)
+        return jsonify(state_obj.to_json())
