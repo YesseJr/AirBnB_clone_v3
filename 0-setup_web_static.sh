ad581f57ec29bc4ecfa43f0380e6d8c6ac8ce4ee
YesseJr <thonnieyesse@hotmail.com>
2022-04-18 13:39:41 +0300

    API

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
-# Sets up webservers for deployment: (Run script on both servers)
-# If not done, does the following:
-#     installs Nginx; creates folders /data/, /data/web_static/,
-#     /data/web_static/releases/, /data/web_static/shared,
-#     /data/web_static/releases/test
-#     /data/web_static/releases/test/index.html (with some content)
-
-ADD_WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Test index.html file to test Nginx config" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "35i $ADD_WEBSTATIC" /etc/nginx/sites-available/default
-sudo service nginx start
+# sets up your web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
