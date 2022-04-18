6dc0c37eaa4967572c0a63ff0837077a191fef01
YesseJr <thonnieyesse@hotmail.com>
2022-04-18 14:25:42 +0300

    States view

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,76 +2,103 @@
 """
 Module containing views for State objects
 """
-from flask import Flask, jsonify, abort, request
-from models import storage
 from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
 from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_state():
+def get_states():
     """
-        return state in json form
+    Retrieves the list of all State objects
+    Returns:
+        A list of dictionaries representing State Objects in JSON format.
     """
-    state_list = [s.to_dict() for s in storage.all('State').values()]
-    return jsonify(state_list)
+    states = []
+    for state in storage.all("State").values():
+        states.append(state.to_dict())
+    return jsonify(states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state_id(state_id):
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_state_by_id(state_id=None):
+    """ Retrieves a state object by state_id
+        Args:
+            state_id (str): The UUID4 string representing a State object.
+        Returns:
+            A state object in JSON format.
+            Raise 404 error if no match for state_id is found.
     """
-        return state and its id using http verb GET
-    """
-    state = storage.get("State", state_id)
-    if state is None:
+    if state_id:
+        for state in storage.all("State").values():
+            if state.id == state_id:
+                return jsonify(state.to_dict())
         abort(404)
-    return jsonify(state.to_dict())
 
 
-@app_views.route(
-    '/states/<state_id>',
-    methods=['DELETE'],
-    strict_slashes=False)
-def delete_state(state_id):
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state_by_id(state_id=None):
+    """ Deletes a state object by state_id
+        Args:
+            state_id (str): The UUID4 string representing a State object.
+        Returns:
+            An empty dictionary with the status code 200.
+            Raise 404 error if no match for state_id is found.
     """
-        delete state obj given state_id
-    """
-    state = storage.get("State", state_id)
-    if state is None:
+    state_obj = storage.get("State", state_id)
+    if state_obj is None:
         abort(404)
-    state.delete()
+    state_obj.delete()
     storage.save()
     return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
+    """ Creates a state object
+        Returns:
+            A dictionary of the state object with status code 201.
+            If HTTP body request is not valid JSON, raise a 400 error with the
+                message "Not a JSON".
+            If dictionary doesn't contain the key name, raise a 400 error with
+                message "Missing name".
     """
-        create new state obj
-    """
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-    elif "name" not in request.get_json():
-        return jsonify({"error": "Missing name"}), 400
-    else:
-        obj_data = request.get_json()
-        obj = State(**obj_data)
-        obj.save()
-        return jsonify(obj.to_dict()), 201
+    if request.json is None:
+        return "Not a JSON", 400
+    fields = request.get_json()
+    if fields.get('name') is None:
+        return "Missing name", 400
+    new_state = State(**fields)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<states_id>', methods=['PUT'], strict_slashes=False)
-def update_state(states_id):
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_state(state_id=None):
+    """ Updates a state object by state_id
+        Args:
+            state_id (str): The UUID4 string representing a State object.
+        Returns:
+            A state object in JSON format with the status code 200.
+            If no object with matching state_id is found, 404 error is raised.
+            If HTTP body request is not valid JSON, raise a 400 error with the
+                message "Not a JSON".
+            If dictionary doesn't contain the key name, raise a 400 error with
+                message "Missing name".
     """
-        update existing state object
-    """
-    if not request.get_json():
-        return jsonify({"error": "Not a JSON"}), 400
-
-    obj = storage.get("State", states_id)
-    if obj is None:
+    if request.json is None:
+        return "Not a JSON", 400
+    fields = request.get_json()
+    state_obj = storage.get("State", state_id)
+    if state_obj is None:
         abort(404)
-    obj_data = request.get_json()
-    obj.name = obj_data['name']
-    obj.save()
-    return jsonify(obj.to_dict()), 200
+    for key in fields:
+        if key not in ['id', 'created_at', 'updated_at']:
+            if hasattr(state_obj, key):
+                setattr(state_obj, key, fields[key])
+    state_obj.save()
+    return jsonify(state_obj.to_dict()), 200
